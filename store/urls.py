30681e5c72b04a1aea16fb2d77c50d1f426eac68
lexicon06 <pablo.hehe2@gmail.com>
2024-06-23 22:54:44 -0300

    Extending..

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import index, home
 
 urlpatterns = [
     path("", index),
+    path("home/", home),
 ]
--- store/views.py
+++ store/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the store index.")
+
+
+def home(request):
+    return render(request, 'home.html')
